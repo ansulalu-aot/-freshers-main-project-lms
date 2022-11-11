c73272a78815826948f2cde68fb417dda21c8ff9
ansulalu-aot <ansulalu@aot-technologies.com>
2022-11-11 23:22:47 +0530

    First commit

---
+++ lms-app/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import LoginForm from './components/LoginForm'
+
+function App() {
+  const adminUser = {
+    email: "admin@gmail.com",
+    password: "admin123"
+  }
+  const [user, setUser] = useState({ email: "", password: "" })
+  const [error, setError] = useState("")
+
+  const Login = details => {
+    console.log(details)
+    if (details.email === adminUser.email && details.password === adminUser.password) {
+      console.log("logged in")
+      setUser({
+        email: details.email,
+        password: details.password
+      })
+    } else {
+      console.log("details not match")
+      setError("details not match")
+    }
+  }
+  return (
+    <div className="App">
+      {(user.email !== "") ? (
+        <div className='welcome'>
+          <h2>welcome</h2>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  )
+}
+export default App
