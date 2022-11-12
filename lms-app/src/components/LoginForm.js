bb0c13d09acc4ce7cf82318297776d147d57a733
ansulalu-aot <ansulalu@aot-technologies.com>
2022-11-12 23:06:57 +0530

    Third commit

--- lms-app/src/components/LoginForm.js
+++ lms-app/src/components/LoginForm.js
@@ -12,16 +12,16 @@ function LoginForm({ Login, error }) {
                 <h2>Login</h2>
                 <p>Welcome back! Please enter your details.</p>
                 <div className='form-tab'>
-                    <a href='#Admin'>Admin</a>
-                    <a href='#Student'>Student</a>
+                    <a href='#Admin' style={{textDecoration:"none"}}>Admin</a>
+                    <a href='#Student' style={{textDecoration:"none"}}>Student</a>
                 </div>
                 {(error !== "") ? (<div className='error'>{error}</div>) : ""}
                 <div className='form-group'>
-                    <label htmlFor='email'>Email:</label>
+                    <label htmlFor='email'>Email</label>
                     <input type="email" name='email' id='email' placeholder="Enter your email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} />
                 </div>
                 <div className='form-group'>
-                    <label htmlFor='password'>Password:</label>
+                    <label htmlFor='password'>Password</label>
                     <input type="password" name='password' id='password' placeholder="Enter your password" onChange={(e) => setDetails({ ...details, password: e.target.value })} value={details.password} />
                 </div>
                 <input type='submit' value="Login" />
